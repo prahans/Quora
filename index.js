116e53d2e2b53eb75bec61fee1990fe5c751e764
amazon-demo <prahanspanuhar4@gmial.com>
2026-01-13 11:09:10 +0545

    adding create route

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get("/posts", (req, res) => {
     res.render("index.ejs", {posts});
 })
 
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+})
+
+app.post("/posts", (req, res) => {
+    let{username, content} = req.body
+    posts.push({username, content});
+    res.redirect("/posts");
+})
+
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 });
