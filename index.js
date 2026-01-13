7ff6ad4462c7a90b72f08113d84e68c6c778f945
amazon-demo <prahanspanuhar4@gmial.com>
2026-01-13 15:39:47 +0545

    adding edit route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const {v4 : uuidv4} = require("uuid");
+const methodOverride = require("method-override")
 const app = express();
 
 const port = 8080;
@@ -9,6 +10,7 @@ app.set("view engine", "views");
 app.set("views", path .join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'))
 
 let posts = [
     {
@@ -47,6 +49,19 @@ app.get("/posts/:id", (req, res) => {
     let {id} = req.params;
     let post = posts.find((p) => id == p.id);
     res.render("show.ejs", {post});
+})
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("edit.ejs", {post});
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    post.content = req.body.content;
+    res.redirect("/posts");
 })
 
 app.listen(port, () => {
